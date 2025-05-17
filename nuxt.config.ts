698cbac96306651db07da45f077243dd88167020
sudo-MeerkatDev <meerkat.dev.96@gmail.com>
2025-05-17 16:43:01 +0700

    #

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  nitro: {
+    preset: "cloudflare-pages",
+  },
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+  ],
+});
